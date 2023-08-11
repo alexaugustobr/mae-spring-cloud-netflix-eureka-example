7c578706d054bc775bbc09638750918347369dcf
Alex Silva <alexaugustoti@gmail.com>
2023-08-11 17:59:44 -0300

    Aula 3 - Implementando Eureka Client

--- review-api/src/main/java/com/algaworks/example/review/ReviewApplication.java
+++ review-api/src/main/java/com/algaworks/example/review/ReviewApplication.java
@@ -2,8 +2,10 @@ package com.algaworks.example.review;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ReviewApplication {
 
 	public static void main(String[] args) {
