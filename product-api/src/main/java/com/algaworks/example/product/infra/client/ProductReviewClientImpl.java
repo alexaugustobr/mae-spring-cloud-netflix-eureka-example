50a13add64e1d955a4f18407ff2fb1d0925ef09d
Alex Silva <alexaugustoti@gmail.com>
2023-08-15 14:10:09 -0300

    Aula 8 - Integrando cliente HTTP com Eureka

--- product-api/src/main/java/com/algaworks/example/product/infra/client/ProductReviewClientImpl.java
+++ product-api/src/main/java/com/algaworks/example/product/infra/client/ProductReviewClientImpl.java
@@ -2,30 +2,25 @@ package com.algaworks.example.product.infra.client;
 
 import com.algaworks.example.product.api.ProductReviewClient;
 import com.algaworks.example.product.api.ReviewModel;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Component
 public class ProductReviewClientImpl implements ProductReviewClient {
 
 	private final Logger logger = LoggerFactory.getLogger(ProductReviewClientImpl.class);
 	private final RestTemplate restTemplate;
+	private final EurekaClient eurekaClient;
 	
-	private final String apiHost = UriComponentsBuilder
-			.fromHttpUrl("http://localhost:8082")
-			.encode()
-			.toUriString();
-	
-	public ProductReviewClientImpl(RestTemplate restTemplate) {
+	public ProductReviewClientImpl(RestTemplate restTemplate, EurekaClient eurekaClient) {
 		this.restTemplate = restTemplate;
+		this.eurekaClient = eurekaClient;
 	}
 
 	@Override
@@ -35,16 +30,37 @@ public class ProductReviewClientImpl implements ProductReviewClient {
 
 		logger.debug("Buscando avaliações");
 		final ReviewModel[] avaliacoes;
-		final var path = "/products/" + productId + "/reviews";
-		final var requestUrl = apiHost + path;
+
+		Optional<InstanceInfo> availableInstance = getAvailableInstance();
+		if (availableInstance.isEmpty()) {
+			return new ArrayList<>();
+		}
+
+		final var path = "products/" + productId + "/reviews";
+		final var requestUrl = availableInstance.get().getHomePageUrl() + path;
 
 		try {
 			avaliacoes = restTemplate.getForObject(requestUrl, ReviewModel[].class, parametros);
 		} catch (Exception e) {
-			logger.error("Erro ao buscar avaliações");
-			throw e;
+			logger.error("Erro ao buscar avaliações", e);
+			return new ArrayList<>();
+		}
+
+		if (avaliacoes == null) {
+			return new ArrayList<>();
 		}
 
 		return Arrays.asList(avaliacoes);
+	}
+
+	private Optional<InstanceInfo> getAvailableInstance() {
+		final var reviewApiApp = eurekaClient.getApplication("review-api");
+		if (reviewApiApp== null) {
+			return Optional.empty();
+		}
+		return reviewApiApp.getInstances()
+				.stream()
+				.filter(p -> p.getStatus().equals(InstanceInfo.InstanceStatus.UP))
+				.findAny();
 	}
 }
